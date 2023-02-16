b33e0f76d325ee4ef39933cca73dfae8baf0c34d
Bazurogg <bazurogg@gmail.com>
2023-02-16 15:54:35 +0100

    Update folder
    cleaning files

--- exo_php1.php
+++ exo_php1.php
@@ -1,10 +1,39 @@
+<style>   
+@import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
+
+* {
+  font-family: 'Open Sans', sans-serif;
+}
+
+body {
+  background-color: #F6E3CE;
+}
+
+h1{
+  color: #F79F81;
+  text-transform: uppercase;
+}
+
+h2{
+  color: #DF3A01;
+  text-decoration: underline;
+}
+
+fieldset{
+    background-color: white;
+}
+</style>
+
+
 <!-- 09.02.2023 -->
 
 <h1>Exercice 1</h1>
 
 <p>Soit la phrase « Notre formation DL commence aujourd'hui ». Ecrire un algorithme permettant de compter le nombre de caractères contenus dans cette phrase (espaces inclus).</p>
 
-<h2>Résultat</h2>
+<fieldset>
+
+<legend><h2>Résultat</h2></legend>
 
 <?php
 
@@ -14,11 +43,15 @@ echo "La phrase \" $phrase \" contient $nbCaracteres caractères.<br>";
 
 ?>
 
+</fieldset>
+
 <h1>Exercice 2</h1>
 
 <p>A partir de la phrase de l’exercice 1, écrire l’instruction permettant de compter le nombre de mots contenus dans celle-ci.</p>
 
-<h2>Résultat</h2>
+<fieldset>
+    
+<legend><h2>Résultat</h2></legend>
 
 <?php
 
@@ -28,11 +61,15 @@ echo "La phrase \" $phrase \" contient $nbDeMots mots.<br>";
 
 ?>
 
+</fieldset>
+
 <h1>Exercice 3</h1>
 
 <p>A partir de la phrase de l’exercice 1, écrire l’instruction permettant de remplacer le mot « aujourd’hui » par le mot « demain ». Afficher l’ancienne et la nouvelle phrase.</p>
 
-<h2>Résultat</h2>
+<fieldset>
+    
+<legend><h2>Résultat</h2></legend>
 
 <?php
 
@@ -44,11 +81,15 @@ echo "$phrase2.<br>";
 
 ?>
 
+</fieldset>
+
 <h1>Exercice 4</h1>
 
 <p>Ecrire un algorithme permettant de savoir si une phrase est palindrome.</p>
 
-<h2>Résultat</h2>
+<fieldset>
+    
+<legend><h2>Résultat</h2></legend>
 
 
 <?php
@@ -75,14 +116,16 @@ else{
 
 ?>
 
+</fieldset>
 
 <h1>Exercice 5</h1>
 
 <p>Ecrire un algorithme qui déclare une valeur en francs et qui la convertit en euros.
 Attention, la valeur générée devra être arrondie à 2 décimales.</p>
 
-<h2>Résultat</h2>
-
+<fieldset>
+    
+<legend><h2>Résultat</h2></legend>
 
 <?php
 
@@ -93,12 +136,15 @@ echo "\"$franc\" francs = \"$euro\" euros.<br>";
 
 ?>
 
+</fieldset>
 
 <h1>Exercice 6</h1>
 
 <p>Ecrire un algorithme permettant de calculer un montant de facture à régler à partir de la quantité d’articles, son prix hors taxe et un taux de TVA (exprimé en décimal. Ex : 20 % -> 0.2)</p>
 
-<h2>Résultat</h2>
+<fieldset>
+    
+<legend><h2>Résultat</h2></legend>
 
 <?php
 
@@ -112,6 +158,8 @@ echo "vous devez régler la somme de $prixTotalTtc euros.<br>";
 
 ?>
 
+</fieldset>
+
 <h1>Exercice 7</h1>
 
 <p>Ecrire un algorithme permettant de renvoyer la catégorie d’un enfant en fonction de son âge :</p>
@@ -124,7 +172,9 @@ echo "vous devez régler la somme de $prixTotalTtc euros.<br>";
     </ul>
 <p>Si la catégorie n’est pas gérée, merci de le préciser.</p>
 
-<h2>Résultat</h2>
+<fieldset>
+    
+<legend><h2>Résultat</h2></legend>
 
 <?php
 
@@ -135,27 +185,28 @@ function categorie($age){
         return "pupille";
     } elseif ($age >= 10 && $age <= 11){
         return "minime";   
-    } elseif ($age >= 12){
+    } elseif ($age <= 12){
         return "cadet";
     } else {
         return "catégorie non gérée";
     }
 }
 
-$age = "12";
+$age = "9";
 
 echo "L'enfant qui a " . $age . " ans appartient à la catégorie : " . categorie($age) .".<br>";
 
-
 ?>
 
+</fieldset>
 
 <h1>Exercice 8</h1>
 
 <p>Ecrire un algorithme qui renvoie la table de multiplication d’un nombre passé en paramètre sous la forme :</p>
 
-
-<h2>Résultat</h2>
+<fieldset>
+    
+<legend><h2>Résultat</h2></legend>
 
 <?php
 
@@ -168,9 +219,10 @@ for ($i = 1; $i <= 10;$i++){
     
     echo "$nb * $i = $produit" ."<br>"; 
 }
+
 ?>
 
-<p>Solution avec la boucle while.</p> 
+<p>Solution avec la boucle "While".</p> 
 
 <?php
 
@@ -186,13 +238,16 @@ while ($i <= 10){
 }
 
 ?>
-
+</fieldset>
 
 <h1>Exercice 9</h1>
 
+
 <p>Nous souhaitons savoir si une personne est imposable en fonction de son âge et de son sexe.Si la personne est une femme dont l’âge est compris entre 18 et 35 ans ou si c’est un homme de plus de 20 ans, alors celle-ci est imposable (sinon ce n’est pas le cas, « non imposable »).</p>
 
-<h2>Résultat</h2>
+<fieldset>
+
+<legend><h2>Résultat</h2></legend>
 
 <?php
 
@@ -214,11 +269,15 @@ echo "Cette personne est " .impot($age, $sexe).".<br>";
 
 ?>
 
+</fieldset>
+
 <h1>Exercice 10</h1>
 
 <p>A partir d’un montant à payer et d’une somme versée pour régler un achat, écrire l’algorithme qui affiche à un utilisateur un rendu de monnaie en nombre de billets de 10 € et 5 €, de pièces de 2 € et 1 €.</p>
 
-<h2>Résultat</h2>
+<fieldset>
+
+<legend><h2>Résultat</h2></legend>
 
 <?php
 
@@ -250,13 +309,17 @@ Monnaie($aPayer, $sommeVersee);
 
 ?>
 
+</fieldset>
+
 <!-- 10.02.2023 -->
 
 <h1>Exercice 11</h1>
 
 <p>Initialiser un tableau de x marques de voitures. Ecrire un algorithme permettant de parcourir ce tableau et d’en afficher le contenu (une marque de voiture par ligne). Il est également demandé d’afficher le nombre de marques de voitures présentes dans le tableau.</p>
 
-<h2>Résultat</h2>
+<fieldset>
+
+<legend><h2>Résultat</h2></legend>
 
 <?php
 
@@ -276,6 +339,7 @@ echo '</ul>';
 
 ?>
 
+</fieldset>
 
 <h1>Exercice 12</h1>
 
@@ -284,20 +348,14 @@ echo '</ul>';
 respective (français ➔ « Salut », anglais ➔ « Hello », espagnol ➔ « Hola »)<br>
 Exemple : tableau ➔ Mickaël -> FRA, Virgile -> ESP, Marie-Claire -> ENG</p>
 
-<h2>Résultat</h2>
+<fieldset>
+
+<legend><h2>Résultat</h2></legend>
 
 <?php
 
-// $bjr = array('bonjour', 'hola', 'hello');
 $ppl = array('mickaël' => 'bonjour' , 'virgile' => 'hola' , 'marie-claire' => 'hello');
 
-// function sayHello ($ppl, $bjr){
-    //     if ($ppl == "fr"){
-//         echo key($bjr);
-//     }
-// }
-// var_dump($bjr);
-// echo ".<br>";
 var_dump($ppl);
 echo ".<br>";
 
@@ -307,11 +365,15 @@ foreach ($ppl as $ppl => $greetings){
 
 ?>
 
+</fieldset>
+
 <h1>Exercice 13</h1>
 
 <p>Calculer la moyenne générale d'un élève dont les notes sont renseignées dans un tableau (pas de coefficient). Elle devra être affichée avec 2 décimales.</p>
 
-<h2>Résultat</h2>
+<fieldset>
+
+<legend><h2>Résultat</h2></legend>
 
 <?php
 
@@ -321,26 +383,25 @@ $nbDeNotes = count($notes);
 $sumNotes = array_sum($notes);
 $moyNotes = round($sumNotes / $nbDeNotes, 2);
 
-
-// foreach ($notes as $notes){
-//     echo $notes .', ';
-// } Liste chaque éléments du tableau.
-
 echo "Les notes obtenues pas l'élèves sont : " .$listeNotes ."."."<br>";
 
-echo "Sa moyenne générale est de :" .$moyNotes ."."."<br>";
+echo "Sa moyenne générale est de :" .$moyNotes."."."<br>";
 
 ?>
+
+</fieldset>
 
 <h1>Exercice 14</h1>
 
 <p>Calculer l'âge exact d'une personne à partir de sa date de naissance (en années, mois, jours).</p>
 
-<h2>Résultat</h2>
+<fieldset>
+
+<legend><h2>Résultat</h2></legend>
 
 <?php
 
-// TEST AFFICHAGE DATE DU JOUR :
+// TEST FONCTIONNEMENT AFFICHAGE DATE DU JOUR :
 // $aujourdhui = new DateTime('now');
 // echo  $aujourdhui->format('Y-m-d H:i:s');
 
@@ -350,8 +411,9 @@ $age = date_diff(date_create($birthDate), date_create($today));
 
 echo "Age de la personne : ". $age->format('%y ans, %m mois, et %d jours');
 
-
 ?>
+
+</fieldset>
 
 <h1>Exercice 15</h1>
 
@@ -360,8 +422,9 @@ Instancier 2 personnes et afficher leurs informations : nom, prénom et âge.<br
 $p1 = new Personne("DUPONT", "Michel", "1980-02-19") ;<br>
 $p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17") ;</p>
 
-<h2>Résultat</h2>
+<fieldset>
 
+<legend><h2>Résultat</h2></legend>
 
 <?php
 
@@ -394,15 +457,12 @@ class Personne {
     }
 }
 
-
 $p1 = new Personne("DUPONT", "Michel","1980-02-19");
 $p2 = new Personne("DUCHEMIN", "Alice","1985-01-17");
 
 echo $p1->name() ." ". $p1->firstname() ." a ".  $p1->age()."<br>";
 echo $p2->name() ." ". $p2->firstname() ." a ".  $p2->age()."<br>";
 
-
 ?>
 
-
-
+</fieldset>
